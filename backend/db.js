63b495bf8876e302bee3e13bec5faf7c906f4341
PoornaPrakash8 <poornaprakash@ieee.org>
2026-02-16 14:56:01 +0530

    databse acess

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,17 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Poorna@123", // put password if you have one
-  database: "evexa"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
   if (err) {
     console.error("❌ MySQL connection error:", err.message);
-    process.exit(1); // STOP server if DB fails
+    process.exit(1);
   }
   console.log("✅ MySQL Connected");
 });
