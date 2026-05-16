caed6e8ce8df30cc9ee63119f3ec4346977df4da
PoornaPrakash8 <poornaprakash@ieee.org>
2026-05-17 00:46:35 +0530

    removed unused react files

--- frontend/src/App.js
+++
@@ -1,7 +0,0 @@
-import CalendarBooking from "./pages/CalendarBooking";
-
-function App() {
-  return <CalendarBooking />;
-}
-
-export default App;
--- frontend/src/services/api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://evexa-production.up.railway.app/api"
-});
